17861eb96827522751367eb3ab11b3109d973382
emeric_martineau <bubulemaster@yahoo.fr>
2014-06-11 13:04:31 +0200

    Red key and Gem object rewrite (better compatibility with original game for red key)

--- open-jill-object-background/src/main/java/org/jill/game/entities/obj/RedKeyManager.java
+++ open-jill-object-background/src/main/java/org/jill/game/entities/obj/RedKeyManager.java
@@ -1,79 +1,21 @@
 package org.jill.game.entities.obj;
 
-import org.jill.game.entities.obj.abs.AbstractSynchronisedImageObjectEntity;
-import org.jill.openjill.core.api.entities.ObjectEntity;
-import org.jill.openjill.core.api.entities.ObjectParam;
-import org.jill.openjill.core.api.message.EnumMessageType;
-import org.jill.openjill.core.api.message.object.ObjectListMessage;
-import org.jill.openjill.core.api.message.statusbar.StatusBarTextMessage;
-import org.jill.openjill.core.api.message.statusbar.inventory.
-        InventoryItemMessage;
-import org.jill.openjill.core.api.message.statusbar.inventory.
-        InventoryPointMessage;
-
 /**
- * Rockkey for mapdoor.
+ * Redkey for door.
  *
  * @author Emeric MARTINEAU
  */
-public final class RedKeyManager extends AbstractSynchronisedImageObjectEntity {
-
+public final class RedKeyManager extends AbstractKeyManager {
     /**
-     * Message to display in bottom of screen.
+     * To know if message must be display.
      */
-    private StatusBarTextMessage msg;
-
-    /**
-     * Inventory object to add.
-     */
-    private InventoryItemMessage inventory;
-
-    /**
-     * Point.
-     */
-    private InventoryPointMessage point;
-
-    /**
-     * To remove this object from object list.
-     */
-    private ObjectListMessage killme;
-
-    /**
-     * Default constructor.
-     *
-     * @param objectParam object parameter
-     */
-    @Override
-    public void init(final ObjectParam objectParam) {
-        super.init(objectParam);
-
-        String textMsg = getConfString("msg");
-        int textColor = getConfInteger("msgColor");
-        int textTime = getConfInteger("msgTime");
-
-        this.msg = new StatusBarTextMessage(textMsg, textTime, textColor);
-
-        this.inventory = new InventoryItemMessage(
-                getConfString("inventory"), true);
-
-        // Remove me from list of object (= kill me)
-        this.killme = new ObjectListMessage(this, false);
-
-        // Point
-        this.point = new InventoryPointMessage(getConfInteger("point"),
-                true);
-    }
+    private static boolean messageDisplayRedKeyMessage = true;
 
     @Override
-    public void msgTouch(final ObjectEntity obj) {
-        if (obj.isPlayer()) {
-            this.messageDispatcher.sendMessage(EnumMessageType.INVENTORY_ITEM,
-                this.inventory);
-            this.messageDispatcher.sendMessage(EnumMessageType.INVENTORY_POINT,
-                this.point);
-            this.messageDispatcher.sendMessage(
-                EnumMessageType.MESSAGE_STATUS_BAR, msg);
-            this.messageDispatcher.sendMessage(EnumMessageType.OBJECT, killme);
-        }
+    protected boolean getDisplayMessage() {
+        boolean oldValue = this.messageDisplayRedKeyMessage;
+        this.messageDisplayRedKeyMessage = false;
+
+        return oldValue;
     }
 }
