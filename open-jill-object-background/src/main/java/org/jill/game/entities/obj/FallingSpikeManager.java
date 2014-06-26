fff55af641841c42e9aa160f4bcbd82c8416d053
emeric_martineau <bubulemaster@yahoo.fr>
2014-06-26 19:14:23 +0200

    Add FallingSpike object support

---
+++ open-jill-object-background/src/main/java/org/jill/game/entities/obj/FallingSpikeManager.java
@@ -0,0 +1,116 @@
+package org.jill.game.entities.obj;
+
+import java.awt.image.BufferedImage;
+import org.jill.game.entities.obj.abs.AbstractHitPlayerObjectEntity;
+import org.jill.game.entities.obj.util.UtilityObjectEntity;
+import org.jill.openjill.core.api.entities.BackgroundEntity;
+import org.jill.openjill.core.api.entities.ObjectEntity;
+import org.jill.openjill.core.api.entities.ObjectParam;
+import org.jill.openjill.core.api.message.EnumMessageType;
+import org.jill.openjill.core.api.message.InterfaceMessageGameHandler;
+import org.jill.openjill.core.api.message.object.ObjectListMessage;
+
+/**
+ * Bonus item manager.
+ *
+ * @author Emeric MARTINEAU
+ */
+public final class FallingSpikeManager extends AbstractHitPlayerObjectEntity
+    implements InterfaceMessageGameHandler {
+
+    /**
+     * Picture array.
+     */
+    private BufferedImage images;
+
+    /**
+     * To remove this object from object list.
+     */
+    private ObjectListMessage killme;
+
+    /**
+     * Size of falling.
+     */
+    private int fallingSpeed;
+
+    /**
+     * Falling speed max.
+     */
+    private int fallingSpeedMax;
+
+    /**
+     * Background map.
+     */
+    private BackgroundEntity[][] backgroundObject;
+
+    /**
+     * Default constructor.
+     *
+     * @param objectParam object paramter
+     */
+    @Override
+    public void init(final ObjectParam objectParam) {
+        super.init(objectParam);
+
+        this.messageDispatcher.addHandler(EnumMessageType.TRIGGER, this);
+
+        // Init list of picture
+        int tileIndex = getConfInteger("tile");
+        int tileSetIndex = getConfInteger("tileSet");
+
+        this.images = this.pictureCache.getImage(tileSetIndex, tileIndex);
+
+        this.fallingSpeed = getConfInteger("fallingSpeed");
+        this.fallingSpeedMax = getConfInteger("fallingSpeedMax");
+
+        this.killme = new ObjectListMessage(this, false);
+
+        this.backgroundObject = objectParam.getBackgroundObject();
+    }
+
+    @Override
+    public BufferedImage msgDraw() {
+        return this.images;
+    }
+
+    @Override
+    public void msgTouch(final ObjectEntity obj) {
+        if (obj.isPlayer()) {
+            hitPlayer(obj);
+
+            this.messageDispatcher.sendMessage(EnumMessageType.OBJECT,
+                    this.killme);
+        }
+    }
+
+    @Override
+    public void recieveMessage(final EnumMessageType type,
+        final Object msg) {
+        switch (type) {
+            case TRIGGER:
+                final ObjectEntity switchObj = (ObjectEntity) msg;
+                if (switchObj.getCounter() == this.counter) {
+                    // Start fall
+                    this.ySpeed = this.fallingSpeed;
+                }
+                break;
+            default:
+        }
+    }
+
+    @Override
+    public void msgUpdate() {
+        if (this.ySpeed > 0) {
+
+            if (UtilityObjectEntity.moveObjectDown(this, this.ySpeed,
+                    this.backgroundObject)) {
+                if (this.ySpeed < this.fallingSpeedMax) {
+                    this.ySpeed += this.fallingSpeed;
+                }
+            } else {
+                this.messageDispatcher.sendMessage(EnumMessageType.OBJECT,
+                    this.killme);
+            }
+        }
+    }
+}
