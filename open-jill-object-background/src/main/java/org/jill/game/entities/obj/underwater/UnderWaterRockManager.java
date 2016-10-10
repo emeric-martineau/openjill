0f1f859cf126a594d3e656b7f6bf10b2387e2f30
emeric_martineau <bubulemaster@yahoo.fr>
2016-10-10 15:54:15 +0200

    First support of underwater rock (object 40) and bubbles (object 58).

---
+++ open-jill-object-background/src/main/java/org/jill/game/entities/obj/underwater/UnderWaterRockManager.java
@@ -0,0 +1,47 @@
+package org.jill.game.entities.obj.underwater;
+
+import java.awt.image.BufferedImage;
+import org.jill.game.entities.obj.abs.AbstractParameterObjectEntity;
+import org.jill.openjill.core.api.entities.ObjectParam;
+import org.jill.openjill.core.api.keyboard.KeyboardLayout;
+
+/**
+ * Rolling rock.
+ *
+ * @author Emeric MARTINEAU
+ */
+public final class UnderWaterRockManager extends AbstractParameterObjectEntity {
+
+    /**
+     * Picture array.
+     */
+    private BufferedImage image;
+
+    /**
+     * Default constructor.
+     *
+     * @param objectParam object paramter
+     */
+    @Override
+    public void init(final ObjectParam objectParam) {
+        super.init(objectParam);
+
+        // Init list of picture
+        // Init list of picture
+        int tileIndex = getConfInteger("tile");
+        int tileSetIndex = getConfInteger("tileSet");
+
+        this.image = objectParam.getPictureCache()
+                .getImage(tileSetIndex, tileIndex);
+    }
+
+    @Override
+    public void msgUpdate(final KeyboardLayout keyboardLayout) {
+        // TODO create image
+    }
+
+    @Override
+    public BufferedImage msgDraw() {
+        return this.image;
+    }
+}
