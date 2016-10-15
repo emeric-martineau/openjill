02fa06c462a146455bf7de032cf1bac56ee69892
emeric_martineau <bubulemaster@yahoo.fr>
2016-10-15 17:57:01 +0200

    Start move bubble support.

--- open-jill-object-background/src/main/java/org/jill/game/entities/obj/underwater/BubblesManager.java
+++ open-jill-object-background/src/main/java/org/jill/game/entities/obj/underwater/BubblesManager.java
@@ -2,8 +2,12 @@ package org.jill.game.entities.obj.underwater;
 
 import java.awt.image.BufferedImage;
 import org.jill.game.entities.obj.abs.AbstractParameterObjectEntity;
+import org.jill.game.entities.obj.util.UtilityObjectEntity;
+import org.jill.openjill.core.api.entities.BackgroundEntity;
 import org.jill.openjill.core.api.entities.ObjectParam;
 import org.jill.openjill.core.api.keyboard.KeyboardLayout;
+import org.jill.openjill.core.api.message.EnumMessageType;
+import org.jill.openjill.core.api.message.object.ObjectListMessage;
 
 /**
  * Rolling rock.
@@ -18,6 +22,16 @@ public final class BubblesManager extends AbstractParameterObjectEntity {
     private BufferedImage image;
 
     /**
+     * Background map.
+     */
+    private BackgroundEntity[][] backgroundObject;
+
+    /**
+     * Move Y
+     */
+    private int moveY;
+
+    /**
      * Default constructor.
      *
      * @param objectParam object paramter
@@ -27,17 +41,33 @@ public final class BubblesManager extends AbstractParameterObjectEntity {
         super.init(objectParam);
 
         // Init list of picture
-        // Init list of picture
         int tileIndex = getConfInteger("tile");
         int tileSetIndex = getConfInteger("tileSet");
 
         this.image = objectParam.getPictureCache()
                 .getImage(tileSetIndex, tileIndex + getCounter());
+
+        this.backgroundObject = objectParam.getBackgroundObject();
+
+        this.moveY = getConfInteger("moveY");
+
+        if (getWidth() == 0 || getHeight() == 0) {
+            setWidth(this.image.getWidth());
+            setHeight(this.image.getHeight());
+        }
     }
 
     @Override
     public void msgUpdate(final KeyboardLayout keyboardLayout) {
-        // TODO move object
+        // Move up
+        if (!UtilityObjectEntity.moveObjectUp(this, moveY,
+                this.backgroundObject)) {
+            this.messageDispatcher.sendMessage(EnumMessageType.OBJECT,
+                new ObjectListMessage(this, false));
+        }
+
+        // TODO check if water
+        // TODO random update X -1/0/+1
     }
 
     @Override
