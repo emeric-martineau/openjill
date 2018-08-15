c263e56d19cc7e6f7329bdf26450503ca9394771
emeric_martineau <11473190+bubulemaster@users.noreply.github.com>
2018-08-15 17:59:33 +0200

    Add background BaseCliff and BaseShore

---
+++ openjill-background-manager/src/main/java/org/jill/entities/manager/background/BaseCliffBackgroundEntity.java
@@ -0,0 +1,22 @@
+package org.jill.entities.manager.background;
+
+import org.jill.entities.manager.background.config.basetreewater.BaseTreeWaterConfig;
+
+/**
+ * Tree.
+ *
+ * @author Emeric MARTINEAU
+ */
+public final class BaseCliffBackgroundEntity
+        extends AbstractBaseBackgroundEntity<BaseTreeWaterConfig> {
+
+    @Override
+    protected Class<BaseTreeWaterConfig> getConfigClass() {
+        return BaseTreeWaterConfig.class;
+    }
+
+    @Override
+    protected String getConfigFilename() {
+        return "lake.json";
+    }
+}
